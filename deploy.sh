6129aee11967cba8eb9aa2052ebb8429c66a3279
srfoster <lindseyhandley@gmail.com>
2020-06-19 10:54:41 -0700

    new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Get things into a clean state.  Ignore errors if things were already clean
+rm -r deploy-out
 git branch gh-pages &>/dev/null
 git worktree prune
-git worktree add out gh-pages
+git worktree add deploy-out gh-pages
 
 touch out/site_prefix #Temporary hack.  raco website-preview should be smarter about this
 
 #Generate the site
 racket main.rkt 
 
+cp -r out/* deploy-out/
+
 #Deploy it
-cd out/
+cd deploy-out/
 git add . -f
 git commit -m "deploy"
 git push -f origin gh-pages
